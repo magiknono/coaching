1b67beb321a98c71a76a1160b463902e9ccec543
magiknono <arno.cormier@gmail.com>
2014-11-24 14:03:10 +0100

    OK

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -1,7 +1,15 @@
 class CoachingController < ApplicationController
   def answer
     @your_message = params[:query]
+     if @your_message.include?("?")
+    return @reponse = "Silly question, get dressed and go to work!"
 
+  elsif @your_message == "I am going to work right now!"
+    return @reponse = ""
+
+  else
+    return @reponse = "I don't care, get dressed and go to work!"
+  end
   end
 
   def ask
